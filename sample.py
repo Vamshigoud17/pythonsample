5fb29f75455a170f372c97ec593fe75383fc1afd
Vamshigoud17 <155362870+Vamshigoud17@users.noreply.github.com>
2024-12-17 14:52:12 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ num = 2
 while count < n:
     # Check if the number is prime
     is_prime = True
-    for i in range(2, int(num ** 0.5) + 1):  # Check divisibility up to the square root of num
+    for i in range(2, num + 1):  # Check divisibility up to the square root of num
         if num % i == 0:
             is_prime = False
             break
