1cf5b7c555a285a05bcff134a8e024bda038696a
Vamshigoud17 <155362870+Vamshigoud17@users.noreply.github.com>
2024-12-17 14:54:08 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,15 +1,15 @@
-n = int(input("Enter the number of prime numbers you want to print: "))
+n = int(input("Enter the number of odd prime numbers you want to print: "))
 count = 0
-num = 2
+num = 3  # Start from 3 because we want odd primes (2 is the only even prime)
 
 while count < n:
     # Check if the number is prime
     is_prime = True
-    for i in range(2, num + 1):  # Check divisibility up to the square root of num
+    for i in range(2, int(num ** 0.5) + 1):  # Check divisibility up to the square root of num
         if num % i == 0:
             is_prime = False
             break
     if is_prime:
         print(num)
         count += 1
-    num += 1
+    num += 2  # Increment by 2 to check only odd numbers (even numbers can't be prime except 2)
