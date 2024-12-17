5721b6c55adff91627a2edd2ba8b9c09d739dc84
Vamshigoud17 <155362870+Vamshigoud17@users.noreply.github.com>
2024-12-17 14:50:23 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,10 +1,11 @@
 n = int(input("Enter the number of prime numbers you want to print: "))
 count = 0
 num = 2
+
 while count < n:
     # Check if the number is prime
     is_prime = True
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num ** 0.5) + 1):  # Check divisibility up to the square root of num
         if num % i == 0:
             is_prime = False
             break
